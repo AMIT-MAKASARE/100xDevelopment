517bb79096891abde582e99f237a18b40c02e414
AMIT-MAKASARE <amitmakasare747@gmail.com>
2025-01-07 06:30:47 +0000

    added some code of 100xdev week 2

--- mernStack/week-5/week-02/2.1/2.2/main.js
+++ mernStack/week-5/week-02/2.1/2.2/main.js
@@ -1,20 +1,34 @@
-const { log } = require("console");
+// const { log } = require("console");
 
-class rectangle{
-    constructor(width,height,color){
-        this.width=width;
-        this.height=height;
-        this.color=color;
-    }
-    area(){
-        const area=this.width*this.height;
-        return area
-    }
-    paint(){
-        console.log(`painting with ${this.color} `);
+const { log } = require("console")
+
+// class rectangle{
+//     constructor(width,height,color){
+//         this.width=width;
+//         this.height=height;
+//         this.color=color;
+//     }
+//     area(){
+//         const area=this.width*this.height;
+//         return area
+//     }
+//     paint(){
+//         console.log(`painting with ${this.color} `);
         
-    }
+//     }
+// }
+// const rect=new rectangle(2,4)
+// const area=rect.area()
+// console.log(area);
+
+// let user=["amit","sumit","shubham"]
+// for(let i=0;i<3;i++){
+//     console.log(user[i]);
+// }
+
+let user={
+    name:"amit",
+    age:"22",
+    gender:"male"
 }
-const rect=new rectangle(2,4)
-const area=rect.area()
-console.log(area);
+console.log('hello'+user.name+" your age is "+user.age+" and your gender is "+user.gender)
